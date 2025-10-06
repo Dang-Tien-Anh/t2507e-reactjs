48e084061ff3ae307b655f835db72f228238b638
Dang-Tien-Anh <tienanh.ums@gmail.com>
2025-10-06 21:18:56 +0700

    test

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      NAVIGATION
+      Section 1
+      Section 2
+      Footer
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+        <footer className="bg-dark text-white text-center p-3 at -4">
+            <p>&copy; 2024 skibidi app all rights reservled!!!</p>
+        </footer>
+    );
+}
+export default Footer;
